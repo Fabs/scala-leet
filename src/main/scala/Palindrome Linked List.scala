8697b2b246bb47cb6e3d8798b2e7dbcd35d8670e
Fabu <fabun@amazon.co.jp>
2022-03-02 21:13:12 +0900

    Simple ineficient solution

---
+++ src/main/scala/Palindrome Linked List.scala
@@ -0,0 +1,25 @@
+class ListNode(_x: Int = 0, _next: ListNode = null) {
+  var next: ListNode = _next
+  var x: Int = _x
+}
+
+// @solution-sync:begin
+object Solution {
+    def isPalindrome(head: ListNode): Boolean = {
+      val list = toList(head)
+      val listReverse = toList(head).reverse
+
+      list == listReverse
+    }
+
+    def toList(head: ListNode): List[Int] = {
+      var list = List.empty[Int]
+      var current = head
+      while(current != null) {
+        list = current.x +: list
+        current = current.next
+      }
+
+      list
+    }
+}
