f639ea094fef22f929a1702c3ab3c4693f939a62
Fabu <fabun@amazon.co.jp>
2022-03-21 19:11:16 +0900

    Plus One

---
+++ src/main/scala/solutions/Plus One.scala
@@ -0,0 +1,20 @@
+object Solution {
+  def plusOne(digits: Array[Int]): Array[Int] = {
+    val result = digits.reverse.foldLeft((1, Array.empty[Int]))((mem, d) => {
+      val carry = mem._1
+      val res = mem._2
+
+      val sum = d + carry
+      val newCarry = sum / 10
+      val newDigit = sum % 10
+
+      (newCarry, res.prepended(newDigit))
+    })
+
+    if (result._1 == 1) {
+      result._2.prepended(1)
+    } else {
+      result._2
+    }
+  }
+}
