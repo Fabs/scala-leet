f86a17658dee158daf913267e822748cb602f056
Fabu <fabun@amazon.co.jp>
2022-03-22 19:12:05 +0900

    Same Tree

---
+++ src/main/scala/solutions/Same Tree.scala
@@ -0,0 +1,20 @@
+class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+  var value: Int = _value
+  var left: TreeNode = _left
+  var right: TreeNode = _right
+}
+
+// @solution-sync:begin
+object Solution {
+    def isSameTree(p: TreeNode, q: TreeNode): Boolean = {
+      if(p == null && q == null) {
+        return true
+      } else if (p == null || q == null) {
+        return false
+      }
+
+      isSameTree(p.left, q.left) &&
+        isSameTree(p.right, q.right) &&
+          p.value == q.value
+    }
+}
