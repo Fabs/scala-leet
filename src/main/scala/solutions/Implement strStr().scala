0721582af55aa8e0bb315e0e2b28997026168f04
Fabu <fabun@amazon.co.jp>
2022-03-23 19:55:59 +0900

    strstr

---
+++ src/main/scala/solutions/Implement strStr().scala
@@ -0,0 +1,29 @@
+object Solution {
+  def strStr(haystack: String, needle: String): Int = {
+    if (needle == "") {
+      return 0
+    }
+
+    if (needle.length > haystack.length) {
+      return -1
+    }
+
+    var i = 0
+    var j = 0
+    while (i < haystack.length && j < needle.length) {
+      if (haystack(i) == needle(j)) {
+        j += 1
+        i += 1
+
+        if (j == needle.length) {
+          return i - j
+        }
+      } else {
+        i = i + 1 - j
+        j = 0
+      }
+    }
+
+    -1
+  }
+}
