3f458037b7f299fbaf92dc7195032b520511fac0
Fabu <fabun@amazon.co.jp>
2022-03-26 09:39:04 +0900

    The K wekaest

---
+++ src/main/scala/solutions/The K Weakest Rows in a Matrix.scala
@@ -0,0 +1,12 @@
+object Solution {
+  def kWeakestRows(mat: Array[Array[Int]], k: Int): Array[Int] = {
+    mat.zipWithIndex.sortBy(score).map(_._2).take(k)
+  }
+
+  def score(row: (Array[Int], Int)): (Int, Int) = {
+    val soldiers = row._1.takeWhile(_ == 1).length
+    val index = row._2
+
+    (soldiers, index)
+  }
+}
